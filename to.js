faac9bd9ab5c9e4689521de97a85a7ace9cbafea
unknown <kammaldeen5@gmail.com>
2024-10-29 11:27:42 +0000

    to

---
+++ to.js
@@ -0,0 +1,32 @@
+let inputt = document.getElementById('inputter');
+let butter = document.getElementById('but');
+let list = document.getElementById('listItems');
+
+butter.addEventListener("click", addToList);
+
+function addToList() {
+    if (inputt.value.trim() == "") {
+        return; 
+    }
+
+    
+    let newItem = document.createElement('li');
+    newItem.textContent = inputt.value;
+
+  
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.className = 'delete';
+    deleteButton.onclick = function() {
+        list.removeChild(newItem); 
+    };
+
+    
+    newItem.appendChild(deleteButton);
+
+    
+    list.appendChild(newItem);
+
+    
+    inputt.value = '';
+}
